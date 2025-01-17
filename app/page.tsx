47de78fafbb2d8a529cf7eb62c98f77a644e3d2c
Yilikal Tesfaye Kebede <yilikalt.kebede@gmail.com>
2025-01-17 22:45:23 +0300

    feat Add PatientForm component and admin link to homepage; update layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* Remains: OTP Verification */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -12,8 +15,26 @@ export default function Home() {
             height={1000}
             className="mb-12 h-10 w-fit"
           />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2025 CarePulse
+            </p>
+            <Link href={"/?admin=true"} className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src={"/assets/images/onboarding-img.png"}
+        alt={"patient"}
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%] "
+      />
     </div>
   );
 }
