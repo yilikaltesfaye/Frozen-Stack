e30236e87cc038c325201858e0818548e6a4b935
Yilikal Tesfaye Kebede <yilikalt.kebede@gmail.com>
2025-01-21 14:23:38 +0300

    fix: make className prop optional in SubmitButton component

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 import Image from "next/image";
 interface ButtonProps {
   isLoading: boolean;
-  className: string;
+  className?: string;
   children: React.ReactNode;
 }
 
